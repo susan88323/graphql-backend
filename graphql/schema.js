74d133c4fe9ae9bd318ab59854415687ad4bd571
Susan Wang <susanw@dev6.com>
2019-12-13 15:40:33 -0500

    Added user status method

--- graphql/schema.js
+++ graphql/schema.js
@@ -20,6 +20,7 @@ module.exports = buildSchema(`
         login(email: String!, password: String!): AuthData!
         posts(page: Int): PostData!
         post(id: ID!): Post!
+        user: User!
     }
 
     type Post {
@@ -58,6 +59,7 @@ module.exports = buildSchema(`
         createPost(postInput: PostInputData): Post!
         updatePost(id: ID!, postInput: PostInputData): Post!
         deletePost(id: ID!): Boolean
+        updateStatus(status: String): User!
     }
 
     schema {
