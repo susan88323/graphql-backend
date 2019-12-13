b705d5963497150efada5ea7341cc2660213a9bb
Susan Wang <susanw@dev6.com>
2019-12-13 09:58:35 -0500

    Added login resolver and added handle for options method

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,43 +1,70 @@
-const bcrypt = require('bcryptjs');
-const validator = require('validator');
+const bcrypt = require("bcryptjs");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
 
-const User = require('../models/user');
+const User = require("../models/user");
 
 module.exports = {
-    hello() {
-        return {
-            text: 'Hello World!',
-            views: 1245
-        };
-    },
+  hello() {
+    return {
+      text: "Hello World!",
+      views: 1245
+    };
+  },
 
-    createUser: async function({ userInput }, req) {
-        const errors = [];
-        if (!validator.isEmail(userInput.email)) {
-            errors.push({ message: 'Email is invalid.' });
-        }
-        if (validator.isEmpty(userInput.password) || validator.isLength(userInput.password, { min: 5 })) {
-            errors.push({ message: 'Password too short!' });
-        }
-        if (errors.length > 0) {
-            const error = new Error('Invalid input.');
-            error.data = errors;
-            error.code = 422;
-            throw error;
-        }
-        const existingUser = await User.findOne({email: userInput.email});
-        if (existingUser) {
-            const error = new Error('User exists already!');
-            throw error;
-        }
-        const hashedPw = await bcrypt.hash(userInput.password, 12);
-        const user = new User({
-            email: userInput.email,
-            name: userInput.name,
-            password: hashedPw
-        });
-        const createdUser = await user.save();
-
-        return { ...createdUser._doc, _id: createdUser._id.toString() };
+  createUser: async function({ userInput }, req) {
+    const errors = [];
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: "Email is invalid." });
     }
+    if (
+      validator.isEmpty(userInput.password) ||
+      validator.isLength(userInput.password, { min: 5 })
+    ) {
+      errors.push({ message: "Password too short!" });
+    }
+    if (errors.length > 0) {
+      const error = new Error("Invalid input.");
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
+    const existingUser = await User.findOne({ email: userInput.email });
+    if (existingUser) {
+      const error = new Error("User exists already!");
+      throw error;
+    }
+    const hashedPw = await bcrypt.hash(userInput.password, 12);
+    const user = new User({
+      email: userInput.email,
+      name: userInput.name,
+      password: hashedPw
+    });
+    const createdUser = await user.save();
+
+    return { ...createdUser._doc, _id: createdUser._id.toString() };
+  },
+  login: async function({ email, password }) {
+    const user = await user.findOne({ email: email });
+    if (!user) {
+      const error = new Error("User not found.");
+      error.code = 401;
+      throw error;
+    }
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      const error = new Error("Password is incorrect.");
+      error.code = 401;
+      throw error;
+    }
+    const token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        email: user.email
+      },
+      "somesecret",
+      { expiresIn: "1h" }
+    );
+    return { token: token, userId: user._id.toString() };
+  }
 };
