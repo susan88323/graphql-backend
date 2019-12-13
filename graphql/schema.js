10defb4cbde4193540c7a4aeb3bb98255f95b9d7
Susan Wang <susanw@dev6.com>
2019-12-13 09:38:27 -0500

    First commit

---
+++ graphql/schema.js
@@ -0,0 +1,51 @@
+const { buildSchema } = require('graphql');
+
+module.exports = buildSchema(`
+    type TestData {
+        text: String!
+        views: Int!
+    }
+
+    type AuthData {
+        token: String!
+        userId: String!
+    }
+
+    type RootQuery {
+        login(email: String!, password: String!): AuthData!
+    }
+
+    type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        imageUrl: String!
+        creator: User!
+        createdAt: String!
+        updatedAt: String!
+    }
+
+    type User {
+        _id: ID!
+        name: String!
+        email: String!
+        password: String
+        status: String!
+        posts: [Post!]!
+    }
+
+    input UserInputData {
+        email: String!
+        name: String!
+        password: String!
+    }
+
+    type RootMutation {
+        createUser(userInput: UserInputData): User
+    }
+
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+`);
